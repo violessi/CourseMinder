443e342525ece5d2a1a73f28bf878a411a7dc752
Masayuki Inukai <mzinukai@up.edu.ph>
2024-03-14 21:38:51 +0800

    fix: add function

--- src/lib/stores/SemesterStores.ts
+++ src/lib/stores/SemesterStores.ts
@@ -1,9 +1,51 @@
 import { SemDetails, Semester, Subjects } from '$lib/models/types';
-import { get, writable } from 'svelte/store';
+import { writable } from 'svelte/store';
+import { initializeApp } from 'firebase/app';
+import { get, getDatabase, ref, set, type DatabaseReference } from 'firebase/database';
+import { studentId } from '$lib/stores/CurriculumStores'
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyCmwpRzGyoeD-Xuh6Cuh1Agbsxw31Uekhk',
+    authDomain: 'courseminder-dev.firebaseapp.com',
+    databaseURL: 'https://courseminder-dev-default-rtdb.asia-southeast1.firebasedatabase.app',
+    projectId: 'courseminder-dev',
+    storageBucket: 'courseminder-dev.appspot.com',
+    messagingSenderId: '274860730108',
+    appId: '1:274860730108:web:b7f706a51ee7a79dbd1979',
+    measurementId: 'G-1T6H3BFHRR',
+};
+
+// Get copy of student Id
+let studentnumber = '';
+studentId.subscribe((value) => {
+    studentnumber = value;
+});
+
+// Initialize Firebase and get database
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
 
 function initStore() {
     const store = writable<Semester[]>([]);
     const { subscribe, set, update } = store;
+
+    async function isExistingSemester(semesterRef : DatabaseReference) {
+        // Check if data for the semester already exists
+        const snapshot = await get(semesterRef);
+        if (snapshot.exists()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    async function writeSemesterData(semesterRef, obg){
+        if (!(await isExistingSemester(semesterRef))){
+            set(semesterRef, obg);
+        }else {
+            throw new Error('Semester already exists');
+        }
+    }
 
     function add({ sem, year }: Omit<SemDetails, 'gwa' | 'units'>) {
         const obg = {
@@ -16,7 +58,8 @@ function initStore() {
             },
             subjects: [],
         } satisfies Semester;
-
+        const semesterRef = ref(db, `semesterData/${studentnumber}/${obg.id}`);
+        writeSemesterData(semesterRef, obg);                  
         update((store) => [...store, obg]);
     }
 
